47ed283e720d865008df7efb4b838398fe3301ec
michaelwilliams <michael.williams@funsational.com>
2011-02-10 14:02:02 -0800

    Add copyright information to files

--- BlogBundle.php
+++ BlogBundle.php
@@ -1,11 +1,27 @@
 <?php
 
+/*
+ * This file is part of the Funsational, Inc Project.
+ *
+ * (c) Funsational, Inc <it@funsational.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ */
+
 namespace Funsational\BlogBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
+/**
+ * FunsationalBlogBundle
+ * 
+ * @author Michael Williams <michael.williams@funsational.com>
+ *
+ */
 class BlogBundle extends Bundle
 {
 }
--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,9 +1,25 @@
 <?php
 
+/*
+ * This file is part of the Funsational, Inc Project.
+ *
+ * (c) Funsational, Inc <it@funsational.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ */
+
 namespace Funsational\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * DefaultController
+ * 
+ * @author Michael Williams <michael.williams@funsational.com>
+ *
+ */
 class DefaultController extends Controller
 {
     public function indexAction()
